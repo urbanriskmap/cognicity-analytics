81e073af6523bf460f86e7887939b6511d2ad37e
ojha-url <mayank.ojha@gmail.com>
2017-12-18 15:50:08 -0500

    Added prod callbackURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   data_server: 'https://data-dev.petabencana.id/',
   auth0_client_id: 'KoKRTjjUC7CpdS770rp29WZyeS73mR1x',
-  auth0_domain: 'dev-riskmap.auth0.com'
+  auth0_domain: 'dev-riskmap.auth0.com',
+  auth0_callbackURL: 'http://dashboard-dev.petabencana.id.s3-ap-southeast-1.amazonaws.com/dashboard'
 };
