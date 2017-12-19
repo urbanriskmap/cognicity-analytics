6fd4c53a7197b75ac02527f466eda9a04c33e249
ojha-url <mayank.ojha@gmail.com>
2017-12-19 15:44:38 -0500

    Removed auth CDN, changed favicon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   data_server: 'https://data-dev.petabencana.id/',
   auth0_client_id: 'KoKRTjjUC7CpdS770rp29WZyeS73mR1x',
   auth0_domain: 'dev-riskmap.auth0.com',
-  auth0_callbackURL: 'http://dashboard-dev.petabencana.id.s3-ap-southeast-1.amazonaws.com/index.html'
+  auth0_callbackURL: 'http://dashboard-dev.petabencana.id/'
 };
