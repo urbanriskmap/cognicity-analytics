59ac81de020b5b84abfd423ee977598a90a00626
ojha-url <mayank.ojha@gmail.com>
2018-01-19 11:21:03 -0500

    UI changes to app component

--- src/app/custom-materials.module.ts
+++ src/app/custom-materials.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CdkTableModule } from '@angular/cdk/table';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatDialog,
+  MatDialogRef,
+  MatRadioModule,
+  MatSelectModule,
+  MatToolbarModule
+} from '@angular/material';
+
 
 @NgModule({
   imports: [
-    MatButtonModule,
-    MatCheckboxModule
   ],
   exports: [
+    NoopAnimationsModule,
+    FormsModule,
+    CdkTableModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatToolbarModule
+  ],
+  providers: [
+    MatDialog,
+    // MatDialogRef
   ]
 })
 export class CustomMaterialsModule { }
