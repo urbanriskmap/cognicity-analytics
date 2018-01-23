ff695986442423fd3502230a3d31023fe8ce09d1
ojha-url <mayank.ojha@gmail.com>
2018-01-23 18:11:13 -0500

    Updated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CogniCity Dashboard';
+  title = 'Dashboard | PetaBencana.id';
 
   constructor(public auth: AuthService) {
     auth.handleAuthentication();
