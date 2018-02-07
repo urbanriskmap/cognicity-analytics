b7fa061f589124bc9fd531f1f3882c88a9bd2db3
Matthew Berryman <matthew@acrossthecloud.net>
2018-02-07 15:46:58 +1100

    fix callback url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
 
   auth0_client_id: 'ApdfZvV1BrxXmwdg6Djrle4m2nav5ub9',
   auth0_domain: 'petabencana.au.auth0.com',
-  auth0_callbackURL: 'http://dashboard-dev.petabencana.id/callback',
+  auth0_callbackURL: 'https://dashboard.petabencana.id/callback',
 
   locales: {
     supportedLanguages: [
